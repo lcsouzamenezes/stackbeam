cc112a95344e54c358f18bfa9c5c2133cf433326
Pierre Raii <pierresraii@gmail.com>
2018-10-06 11:35:08 +0300

    add 404 handling in server.js in test script

--- test/server.js
+++ test/server.js
@@ -13,20 +13,31 @@ const ZeroCrash = require('../libs/zerocrash').install(TOKEN);
 app.use(ZeroCrash.requestHandler());
 
 app.get('/', async (req, res, next) => {
-  let erroredAsyncFunction = async () => {
-    throw new Error('AN ASYNC ERROR HAS OCCURED!');
-  };
+  // console.log('req: ', req);
+  // let erroredAsyncFunction = async () => {
+  //   throw new Error('AN ASYNC ERROR HAS OCCURED!');
+  // };
 
-  await erroredAsyncFunction();
+  // await erroredAsyncFunction();
   // throw new Error('A SYNC ERROR HAS OCCURED');
 
   // fs.readFile('somefile.txt', function (err, data) {
   //   if (err) throw err;
   //   console.log(data);
   // });
+  setTimeout(() => {
+    return res.status(200).json({ message: 'm' });  
+  }, 2000);
 });
 
 app.use(ZeroCrash.errorHandler());
+/**
+ * Catch 404 and forward to error handler
+ */
+app.use((request, response, next) => {
+  response.statusCode = 404;
+  response.end('Not found');
+});
 
 app.use(function onError(err, req, res, next) {
   res.statusCode = 500;
